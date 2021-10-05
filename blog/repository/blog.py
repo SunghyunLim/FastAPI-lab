133fa4469f69b0f5793277c9674be2bd98454cdb
SungHyunLim <sunghyun.lim@gmail.com>
2021-10-05 20:25:15 +0900

    Add blog modification

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import Session
-from .. import models
+from .. import models, schemas
 
 
 def get_all(db: Session):
     blogs = db.query(models.Blog).all()
     return blogs
+
+def create(request: schemas.Blog, db:Session):
+    new_blog = models.Blog(title= request.title, body=request.body, user_id = 1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -16,11 +16,7 @@ def all(db : Session= Depends(get_db)):
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db : Session= Depends(get_db)):
-    new_blog = models.Blog(title= request.title, body=request.body, user_id = 1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destory(id, db : Session= Depends(get_db)):
