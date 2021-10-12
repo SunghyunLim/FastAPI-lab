604017fd513e4564cedc1ec7092140e3d2c652e0
SungHyunLim <sunghyun.lim@gmail.com>
2021-10-13 00:14:23 +0900

    verify Password

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,3 +5,6 @@ pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def bcrypt(password:str):
         return pwd_ctx.hash(password)
+
+    def verify(hashed_password, plain_password):
+        return pwd_ctx.verify(hashed_password, plain_password)
