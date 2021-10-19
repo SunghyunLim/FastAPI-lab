fb27b90507e9e94ca5004c528b3dc6d3473542c8
SungHyunLim <sunghyun.lim@gmail.com>
2021-10-20 00:05:52 +0900

    verify token

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -4,10 +4,11 @@ from . import token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(data: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credtentials",
+        detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    token.verity_token(token, credentials_exception)
+
+    return token.verify_token(data, credentials_exception)
