2c4172ecf3dbe52c25c28a1e4e353c2600d745bf
SungHyunLim <sunghyun.lim@gmail.com>
2021-10-19 23:39:45 +0900

    access token added

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from blog import schemas
 from blog.token import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from .. import schemas, database, models, token
@@ -11,7 +12,7 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request:schemas.Login, db: Session = Depends(database.get_db)):
+def login(request:OAuth2PasswordRequestForm =Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
